8d66eb39869b4227fc67d4127041d605d6c16cbe
Jared Grove <jaredkgrove@gmail.com>
2019-03-01 05:43:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.size == 1
     array_string = array.join
   else
-    array.each_with_index |element, index|
+    array.each_with_index do |element, index|
       if index == 0
         array_string << element
       elsif index == array.size - 1
